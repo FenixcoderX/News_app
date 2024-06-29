83f02420c59148842c7a5173612c6aa973490de3
FenixcoderX <fenixcoder2024@gmail.com>
2024-06-29 15:10:42 +0700

    Add routing and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import NewsList from './pages/NewsList';
+import NewsNew from './pages/NewsNew';
 
 function App() {
+
   return (
     <div className="App-main">
-     News
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<NewsList />} />
+        <Route path="/new" element={<NewsNew />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
