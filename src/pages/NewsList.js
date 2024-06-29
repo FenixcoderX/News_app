57995a67ce30b11deb36f615dc3e7412332f7887
FenixcoderX <fenixcoder2024@gmail.com>
2024-06-29 15:12:25 +0700

    Add NewsList, NewsNew components

---
+++ src/pages/NewsList.js
@@ -0,0 +1,6 @@
+const NewsNew = () => {
+    return <div>Here in future could be logic to show News etc</div>;
+  };
+  
+  export default NewsNew;
+  
---
+++ src/pages/NewsNew.js
@@ -0,0 +1,5 @@
+const NewsNew = () => {
+  return <div>NewsNew</div>;
+};
+
+export default NewsNew;
